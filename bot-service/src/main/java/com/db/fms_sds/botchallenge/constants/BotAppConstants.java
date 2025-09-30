3c942b39995b0e7d0729771a7f82c3dcaf12a6f5
robert8597 <66479327+robert8597@users.noreply.github.com>
2025-09-30 13:39:43 +0200

    Update BotAppConstants.java

--- bot-service/src/main/java/com/db/fms_sds/botchallenge/constants/BotAppConstants.java
+++ bot-service/src/main/java/com/db/fms_sds/botchallenge/constants/BotAppConstants.java
@@ -7,5 +7,12 @@ public abstract class BotAppConstants {
     // TODO Task 0: Set up the host constant
     public static final String HOST = "?";
 
+
+    public static final String SENDER_REFERENCE = "BankOnTech";
+    public static final String SERVICE_CODE = "swift.finplus!pc";
+    public static final String MESSAGE_TYPE = "pacs.008.001.13";
+    public static final String REQUESTOR = "ou=xxx,o=deutdeff,o=swift";
+    public static final String RESPONDER = "ou=xxx,o=bktrus33,o=swift";
+
 }
 
